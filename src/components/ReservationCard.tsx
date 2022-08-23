cde3ca7800f2733ac1889a7a55d0913ab9c43d34
Scott Carew <103097506+scott-carew@users.noreply.github.com>
2022-08-24 08:48:29 +1200

    complete customer card

--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
+import { addCustomer } from '../features/customerSlice';
 import { removeReservation } from '../features/reservationSlice';
+import { v4 as uuid } from 'uuid'
 
 
 interface ReservationCardTypes {
@@ -11,9 +13,14 @@ interface ReservationCardTypes {
 export default function ReservationCard({name, index}: ReservationCardTypes) {
 
   const dispatch = useDispatch()
-
+  
   return ( <div onClick={() => {
     dispatch(removeReservation(index))
+    dispatch(addCustomer({
+      id: uuid(),
+      name,
+      food: []
+    }))
   }}>
     <div className="reservation-card-container">{name}</div>
 

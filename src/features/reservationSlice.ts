c012fe79fbadf5fcd0fd0187bbf04a37a5df1992
Scott Carew <103097506+scott-carew@users.noreply.github.com>
2022-08-24 06:17:25 +1200

    addReservation action to reservationSlice

--- src/features/reservationSlice.ts
+++ src/features/reservationSlice.ts
@@ -1,6 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
-const initialState = {
+interface ReservationState {
+    value: string[]
+}
+
+const initialState: ReservationState = {
   value: []
 }
 
@@ -8,8 +12,12 @@ export const reservationsSlice = createSlice({
   name: "reservations",
   initialState,
   reducers: {
-
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload)
+    }
   }
 })
+
+export const { addReservation } = reservationsSlice.actions
 
 export default reservationsSlice.reducer

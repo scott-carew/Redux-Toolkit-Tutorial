7f094705533a964f98e5702ffd01846963ff5aff
Scott Carew <103097506+scott-carew@users.noreply.github.com>
2022-08-24 08:09:19 +1200

    remove reducer slice complete

--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { removeReservation } from '../features/reservationSlice';
+
 
 interface ReservationCardTypes {
-  name: string
+  name: string;
+  index: number
 }
 
-export default function ReservationCard({name}: ReservationCardTypes) {
-  return (
+export default function ReservationCard({name, index}: ReservationCardTypes) {
+
+  const dispatch = useDispatch()
+
+  return ( <div onClick={() => {
+    dispatch(removeReservation(index))
+  }}>
     <div className="reservation-card-container">{name}</div>
+
+  </div>
   )
 }
